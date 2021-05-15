e828b19333a03abadbb992e1e8268445671e7eed
VladaGaz <vladamuzyka7@gmail.com>
2021-05-15 22:43:36 +0300

    добавление компонентов Message, NewNote

---
+++ src/components/Message.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="message">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style  scoped>
+.message {
+  text-align: center;
+  padding: 20px;
+}
+</style>
